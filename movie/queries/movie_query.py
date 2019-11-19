07d249f9be099f35f1165226bf55f8e0a879e12d
Kovryga Kirill <kovrygakirill@mail.ru>
2019-11-19 21:11:59 +0300

    refactoring function(logic)

--- movie/queries/movie_query.py
+++ movie/queries/movie_query.py
@@ -3,38 +3,25 @@ from ..models import Movie, Category
 
 class MovieQuery:
     @staticmethod
-    def filterHome(params):
+    def filterRequest(params):
         result = Movie.object.order_by("-created_date")
 
-        if params.get("movies"):
-            result = MovieQuery.filterCategories_detail(params)
-        if params.get("sort"):
-            result = MovieQuery.filterCategories_detail(params)
-
-        return result
-
-    @staticmethod
-    def filterCategories_detail(params):
-        result = Movie.object.order_by("-created_date")
-
-        if params.get("slug"):
-            result = MovieQuery.slugMovie(result, params['slug'])
         if params.get("sort"):
             result = MovieQuery.sortMovie(result, params['sort'])
-        if params.get("movies"):
-            result = MovieQuery.searchMovies(result, params['movies'])
+        if params.get("search"):
+            result = MovieQuery.searchMovies(result, params['search'])
+        if params.get("pk"):
+            result = MovieQuery.pkMovie(result, params['pk'])
+        if params.get("slug"):
+            result = MovieQuery.slugMovie(result, params['slug'])
 
         return result
 
     @staticmethod
-    def searchMovies(scope, title):
-        return scope.filter(movie_name=title)
-
-    @staticmethod
-    def sortMovie(scope, title):
-        if title == 'Alphabet':
+    def sortMovie(scope, sort):
+        if sort == 'Alphabet':
             result = scope.order_by("movie_name")
-        elif title == 'Date':
+        elif sort == 'Date':
             result = scope.order_by("-created_date")
         else:
             result = scope
@@ -42,9 +29,20 @@ class MovieQuery:
         return result
 
     @staticmethod
-    def slugMovie(scope, title):
-        if Category.object.filter(slug=title).exists():
-            category = Category.object.get(slug=title)
+    def searchMovies(scope, search):
+        return scope.filter(movie_name=search)
+
+    @staticmethod
+    def pkMovie(scope, pk):
+        if Movie.object.filter(pk=pk).exists():
+            return scope.get(pk=pk)
+        else:
+            return []
+
+    @staticmethod
+    def slugMovie(scope, slug):
+        if Category.object.filter(slug=slug).exists():
+            category = Category.object.get(slug=slug)
             return scope.filter(category=category)
         else:
-            return scope
+            return []
