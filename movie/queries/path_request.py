bb7fa2d0a1cff0a4c24e4259937339a7ef9cce1e
Kovryga Kirill <kovrygakirill@mail.ru>
2019-12-02 19:47:01 +0300

    changed name method

--- movie/queries/path_request.py
+++ movie/queries/path_request.py
@@ -4,7 +4,7 @@ from django.http.request import QueryDict
 
 class PathRequest:
     @staticmethod
-    def getQueryWithout(path_url):
+    def getQueryWithoutPage(path_url):
         if path_url:
             query_as_dict = QueryDict(path_url).copy()
             if query_as_dict.get('page'):
@@ -15,3 +15,21 @@ class PathRequest:
             return query_as_string
         else:
             return ''
+
+    @staticmethod
+    def getPathQuery(oldPath, newQuery):
+        if oldPath:
+            oldPath_dict = QueryDict(oldPath).copy()
+            newQuery_dict = QueryDict(newQuery).copy()
+
+            if oldPath_dict.keys() & newQuery_dict.keys():
+                replayKey = (oldPath_dict.keys() & newQuery_dict.keys()).pop()
+                oldPath_dict[replayKey] = newQuery_dict[replayKey]
+
+                newPath = urlencode(oldPath_dict)
+            else:
+                newPath = oldPath + "&" + newQuery
+
+            return newPath
+        else:
+            return newQuery
