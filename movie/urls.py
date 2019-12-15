2e5a90eeab27ff97b0c0e7c25828d51459be3d32
Kovryga Kirill <kovrygakirill@mail.ru>
2019-12-15 22:58:41 +0300

    comment out some url

--- movie/urls.py
+++ movie/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 # from videoClub.movie.views import CategoryListView --- WRONG
-from .views import post_Home, detail_movies, detail_category, error_404, random_movie, addLike,addDislike
+from .views import post_Home, detail_movies, detail_category, error_404, random_movie
 
 # handler404 = error_404
 
@@ -9,8 +9,8 @@ urlpatterns = [
     # url(r'^category/(?P<slug>[-\w]+)/$', CategoryDetailView.as_view(), name="category-detail"),
     # url(r'^movies/(?P<pk>[0-9]+)/$', MovieView.as_view(), name="movie_view"),
     url(r'^$', post_Home, name="post_Home"),
-    url(r'^add_like/(?P<pk>\d+)/$', addLike, name="add_like"),
-    url(r'^add_dislike/(?P<pk>\d+)/$', addDislike, name="add_dislike"),
+    # url(r'^add_like/(?P<pk>\d+)/$', addLike, name="add_like"),
+    # url(r'^add_dislike/(?P<pk>\d+)/$', addDislike, name="add_dislike"),
     url(r'^category/(?P<slug>[\w]+)/$', detail_category, name="detail_category"),
     url(r'^movies/(?P<pk>\d+)/$', detail_movies, name="detail_movies"),
     url(r'^random_movie/$', random_movie, name="random_movie"),
