56a58fee355aaa41252aa96966904af44c4cd38d
Kovryga Kirill <kovrygakirill@mail.ru>
2019-12-15 23:36:09 +0300

    test preparation on method authentication

---
+++ tests/admin/authentication/test_authentication.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class AuthUserAddTest(TestCase):
+    def test_addUser_correctData_authSuccessful(self):
+        pass
