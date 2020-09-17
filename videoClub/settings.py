072e1c8d32a24c6adfe925ff54afea4189bebd76
Kovryga Kirill <kovrygakirill@mail.ru>
2020-09-17 22:05:21 +0300

    update email settings

--- videoClub/settings.py
+++ videoClub/settings.py
@@ -148,11 +148,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
