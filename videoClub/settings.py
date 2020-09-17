d2f9fc6fd31efa0bb6cf1a4a2beb078c22173a4a
Kovryga Kirill <kovrygakirill@mail.ru>
2020-09-17 23:26:38 +0300

    update email settings

--- videoClub/settings.py
+++ videoClub/settings.py
@@ -152,7 +152,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_PORT = 587
+EMAIL_PORT = os.environ["EMAIL_PORT"]
 # EMAIL_USE_SSL = False
 
 
