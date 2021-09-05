6a07247c66717ebd8149b65d2e1f3b17f402c473
Kovryga Kirill <kovrygakirill@mail.ru>
2021-09-05 18:20:24 +0300

    update settings for send email

--- videoClub/settings.py
+++ videoClub/settings.py
@@ -150,8 +150,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ["SENDGRID_USERNAME"]
-EMAIL_HOST_PASSWORD = os.environ["SENDGRID_PASSWORD"]
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.environ["API_KEY"]
 EMAIL_PORT = os.environ["EMAIL_PORT"]
 # EMAIL_USE_SSL = False
 
